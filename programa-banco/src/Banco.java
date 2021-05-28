e55eec28788b10e53faa33703f7733ac8d72b4e7
lgmro <lucasgmmro@gmail.com>
2021-05-28 20:05:24 -0300

    Criando variáveis e scanner

---
+++ programa-banco/src/Banco.java
@@ -0,0 +1,20 @@
+//Importações
+import java.util.Scanner;
+
+public class Banco {
+    public static void main (String[] args) {
+        //Declarando variáveis.
+        float balance, value;
+        char opcion;
+        int account;
+
+        //Scanner.
+        Scanner variablenumeric = new Scanner(System.in);
+
+        System.out.print ("Qual o número da sua conta bancária: ");
+
+
+
+    }
+
+}
