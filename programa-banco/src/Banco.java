ba9780fac46838be46f52f523416ef5f0898c663
lgmro <lucasgmmro@gmail.com>
2021-05-28 20:28:22 -0300

    Solicitando informações ao usuário

--- programa-banco/src/Banco.java
+++ programa-banco/src/Banco.java
@@ -1,4 +1,5 @@
 //Importações
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Banco {
@@ -10,8 +11,23 @@ public class Banco {
 
         //Scanner.
         Scanner variablenumeric = new Scanner(System.in);
+        Scanner variabletext = new Scanner(System.in);
 
+        //Solicitando informações ao usuário.
         System.out.print ("Qual o número da sua conta bancária: ");
+        account = variablenumeric.nextInt();
+
+        System.out.print ("Informe o saldo atual da sua conta: R$ ");
+        balance = variablenumeric.nextFloat();
+
+        System.out.print ("Opções: D - para debitar e C - para creditar. ");
+
+        System.out.print ("O que você quer fazer? ");
+        opcion = variabletext.nextLine().charAt(0);
+
+        System.out.print ("Qual o valor a ser debitado ou creditado? ");
+        value = variablenumeric.nextFloat();
+
 
 
 
