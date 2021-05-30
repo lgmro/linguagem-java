d717963395397ef024536ac69b3e09ff1827efdb
lgmro <lucasgmmro@gmail.com>
2021-05-30 14:46:42 -0300

    Criando função creditar

--- programa-banco/src/Funcoes.java
+++ programa-banco/src/Funcoes.java
@@ -1,9 +1,7 @@
 public class Funcoes {
 
-    public String firstname;
 
     public void debit (float x, float y) {
-
         if (x - y > 0) {
             System.out.print(" agora tem o saldo final de: R$ ");
             System.out.println(x - y);
@@ -12,5 +10,11 @@ public class Funcoes {
 
             System.out.println(" não possui saldo suficiente para essa operação.");
         }
+    }
+
+    public void credit (float x, float y) {
+        System.out.print (" agora tem o saldo final de: R$ ");
+        System.out.println (x + y);
+
     }
 }
