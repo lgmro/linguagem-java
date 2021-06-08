fd01d4de6f1acec3a6dfd4c617e040ef1efc66d2
lgmro <lucasgmmro@gmail.com>
2021-06-08 20:26:33 -0300

    Criando classse contatos.

---
+++ programa-agenda/src/lucas/agenda/Contatos.java
@@ -0,0 +1,32 @@
+package lucas.agenda;
+
+public class Contatos {
+    private String nomecontato;
+    private String email;
+    private int telefone;
+
+    public String getNomecontato() {
+        return this.nomecontato;
+    }
+
+    public void setNomecontato(String nomecontato) {
+        this.nomecontato = nomecontato;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getTelefone() {
+        return this.telefone;
+    }
+
+    public void setEmail(int telefone) {
+        this.telefone = telefone;
+    }
+
+}
