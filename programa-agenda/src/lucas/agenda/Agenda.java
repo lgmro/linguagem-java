bb8d94cb497f379c276a9b21a1cdb061d9bbef4b
lgmro <lucasgmmro@gmail.com>
2021-06-08 20:05:11 -0300

    Projeto agenda: criação classe agenda

---
+++ programa-agenda/src/lucas/agenda/Agenda.java
@@ -0,0 +1,15 @@
+package lucas.agenda;
+
+public class Agenda {
+    private String nome;
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome (String nome) {
+        this.nome = nome;
+
+    }
+
+}
